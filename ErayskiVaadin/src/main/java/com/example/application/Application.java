54c3f53aa9219b4b3c20282f2ab4fccde9f7dc69
Eray Seyhan <eray_seyhan@abv.bg>
2021-12-09 22:37:47 +0200

    Vaadin

---
+++ ErayskiVaadin/src/main/java/com/example/application/Application.java
@@ -0,0 +1,22 @@
+package com.example.application;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.vaadin.artur.helpers.LaunchUtil;
+
+@SpringBootApplication
+@NpmPackage(value = "lumo-css-framework", version = "^4.0.10")
+@Theme("flowcrmtutorial")
+@PWA(name = "ErayskiCrud", shortName = "ERI", offlinePath="offline.html", offlineResources = { "./images/offline.png"})
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(Application.class, args));
+    }
+
+}
